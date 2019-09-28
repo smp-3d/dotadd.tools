f616f843658b1983a54328c4241ee2d7df62d6a5
Jonas Ohland <jonas.ohland@gmail.com>
2019-09-28 11:17:26 +0200

    remove built files

--- build/AmbidecodeFormat.d.ts
+++
@@ -1,9 +0,0 @@
-import { ContainerType } from "./ADCFormat";
-import { ParseResults, ConverterOptions } from './Converter';
-export default class AmbidecodeFormat {
-    static getName(): string;
-    static getDescription(): string;
-    static container_type(): ContainerType;
-    static test(obj: Object): Boolean;
-    static parse(obj: Object, filename: string, carry: ParseResults, opts: ConverterOptions): void;
-}
--- build/Logger.js
+++
@@ -1,7 +0,0 @@
-export class Logger {
-    static log(...any) {
-        let self = Logger;
-        if (self.prototype.verbose)
-            console.log(...any);
-    }
-}
